5605094e57e501371d47f7849863090bc8a777e2
RichAubrey <Richmanra18@gmail.com>
2024-11-25 22:37:23 +0200

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mainapp;
+
+import DriverLibra.ac.Driver;
+
+/**
+ *
+ * @author USER
+ */
+public class MainApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Driver dr = new Driver() ;
+        dr.setVisible(true);
+    }
+    
+}
