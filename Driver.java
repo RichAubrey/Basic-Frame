0d25b6da6f35bc168f7e85155b8b4c4566dcd2e5
RichAubrey <Richmanra18@gmail.com>
2024-11-25 22:35:45 +0200

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DriverLibra.ac;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.*;
+
+/**
+ *
+ * @author USER
+ */
+public class Driver extends JFrame {
+  public Driver()
+  {
+      setTitle("My first GUI");
+      setSize(200,250);
+      setDefaultLookAndFeelDecorated(true);
+      setResizable(true);
+      setDefaultCloseOperation(EXIT_ON_CLOSE);
+      setForeground(Color.BLUE);
+      setVisible(true);
+  
+  }
+    
+}
+
